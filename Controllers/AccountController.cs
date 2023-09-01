86eea5df7a50f75fcc66db766dcc74c1bb70a96d
Mohammad Aliyan <aliyan.engineer@gmail.com>
2023-09-01 15:25:35 +0330

    add httpclients

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using System.Data;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Identity.Controllers;
 
@@ -20,11 +22,12 @@ public class AccountController : ApiControllerBase
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly RoleManager<ApplicationRole> _roleManager;
     private readonly IOptionsSnapshot<AppSettings> _settings;
- 
+
+
 
     public AccountController(IMapper mapper,
         UserManager<ApplicationUser> userManager,
-        RoleManager<ApplicationRole> roleManager, 
+        RoleManager<ApplicationRole> roleManager,
         IOptionsSnapshot<AppSettings> settings)
     {
         _mapper = mapper;
@@ -43,14 +46,14 @@ public class AccountController : ApiControllerBase
         }
         var user = _mapper.Map<ApplicationUser>(userModel);
         var result = await _userManager.CreateAsync(user, userModel.Password);
-        var tk =  await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        var tk = await _userManager.GenerateEmailConfirmationTokenAsync(user);
         if (!result.Succeeded)
         {
-          
+
             return BadRequest(result.Errors);
         }
         return Ok(tk);
-        
+
 
     }
 
@@ -58,8 +61,8 @@ public class AccountController : ApiControllerBase
     [HttpPost("AddUserRole")]
     public async Task<IActionResult> AddUserRole(UserRoleModel userRoleModel)
     {
-        
-        var user =await _userManager.FindByIdAsync(userRoleModel.UserId);
+
+        var user = await _userManager.FindByIdAsync(userRoleModel.UserId);
         var role = await _roleManager.FindByIdAsync(userRoleModel.RoleId);
         if (user != null && role != null)
         {
@@ -76,7 +79,7 @@ public class AccountController : ApiControllerBase
     [HaveAccess("admin")]
     public async Task<IActionResult> GetRoleByUserId(string userId)
     {
-        var dd=CurrentUserId;
+        var dd = CurrentUserId;
         var user = await _userManager.FindByIdAsync(userId);
         var res = await _userManager.GetRolesAsync(user);
         return Ok(res);
@@ -142,20 +145,45 @@ public class AccountController : ApiControllerBase
 
     [AllowAnonymous]
     [HttpPost("CofirmEmail")]
-    public async Task<IActionResult> CofirmEmail( ConfirmEmailModel model)
+    public async Task<IActionResult> CofirmEmail(ConfirmEmailModel model)
     {
         var user = await _userManager.FindByEmailAsync(model.Email);
         if (user is null)
         {
             return BadRequest("User not Found");
         }
-        var res = await _userManager.ConfirmEmailAsync(user,model.Token);
+        var res = await _userManager.ConfirmEmailAsync(user, model.Token);
         if (!res.Succeeded)
         {
             return BadRequest("Invalid Token");
         }
 
         return Ok();
+
+    }
+
+    [AllowAnonymous]
+    [HttpGet("GetProducts")]
+    public async Task<IActionResult> GetProducts()
+    {
+
+        var d = GetListProducts();
+        return Ok(d);
+    }
+
+    public class ProductModel
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public decimal price { get; set; }
+    }
+
+    private List<ProductModel> GetListProducts()
+    {
+        using var client = new HttpClient();
+        var res = client.GetStringAsync("https://fakestoreapi.com/products").Result;
+        var response = JsonSerializer.Deserialize<List<ProductModel>>(res);
+        return response;
 
     }
 
